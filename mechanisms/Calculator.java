247881c6a55ba57431db8fb70290cbda3a30cf74
NickyLogan <laurentiusdominick@yahoo.com>
2017-04-08 20:28:42 +0700

    Update on Calculator
    overloaded methods: all
    -> using String as parameter

--- mechanisms/Calculator.java
+++ mechanisms/Calculator.java
@@ -7,23 +7,37 @@ import org.mariuszgromada.math.mxparser.Expression;
  * Created by Nicky on 3/28/2017.
  */
 public class Calculator {
-    public static String integrate(String expression, String lower, String upper){
+    public static double integrate(String expression, String lower, String upper){
         String s = "int(" + expression + ", x, " + lower + ", " + upper + ")";
-        System.out.println(s);
+//        System.out.println(s);
         Expression expr = new Expression(s);
         double d = expr.calculate();
-        return PrettyDouble.beautifyGenerateHTML(d);
+        return d;
     }
-    public static String derivative(String expression, String point){
+    public static double integrate(String expression, double lower, double upper){
+        String s = "int(" + expression + ", x, " + lower + ", " + upper + ")";
+//        System.out.println(s);
+        Expression expr = new Expression(s);
+        double d = expr.calculate();
+        return d;
+    }
+    public static double derivative(String expression, String point){
         String s = "der(" + expression + ", x)";
         Expression pointExpr = new Expression(point);
-        System.out.println(s);
+//        System.out.println(s);
         Expression expr = new Expression(s, new Argument("x", pointExpr.calculate()));
         double d = expr.calculate();
-        return PrettyDouble.beautifyGenerateHTML(d);
+        return d;
     }
-    public static String calculate(String expression){
+    public static double derivative(String expression, double point){
+        String s = "der(" + expression + ", x)";
+//        System.out.println(s);
+        Expression expr = new Expression(s, new Argument("x", point));
+        double d = expr.calculate();
+        return d;
+    }
+    public static double calculate(String expression){
         Expression expr = new Expression(expression);
-        return PrettyDouble.beautifyGenerateHTML(expr.calculate());
+        return expr.calculate();
     }
 }
